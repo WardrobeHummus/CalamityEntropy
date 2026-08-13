45f484f67aadbef8b2bd0e6996cce77a6fc40795
Joshua D'Abreu <joshuadabreu@hotmail.com>
2026-08-14 00:29:11 +0100

    Test Push

--- Content/Items/Weapons/VoidBlade.cs
+++ Content/Items/Weapons/VoidBlade.cs
@@ -11,7 +11,7 @@ namespace CalamityEntropy.Content.Items.Weapons
     {
         public override void SetDefaults()
         {
-            Item.damage = 26;
+            Item.damage = 16;
             Item.crit = 15;
             Item.DamageType = ModContent.GetInstance<TrueMeleeDamageClass>();
             Item.width = 100;
